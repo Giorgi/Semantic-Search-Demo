4a0bb317e07be4f0018ad251476827dbf2a919c4
Giorgi <giorgi@giorgi.dev>
2024-05-30 15:50:38 +0400

    Local embedding search demo

---
+++ SemanticSearchDemo/NewsItem.cs
@@ -0,0 +1,15 @@
+﻿using SmartComponents.LocalEmbeddings;
+
+namespace SemanticSearchDemo;
+
+public class NewsItem
+{
+    public string Link { get; set; }
+    public string Headline { get; set; }
+    public string Category { get; set; }
+    public string ShortDescription { get; set; }
+    public string Authors { get; set; }
+    public DateTime Date { get; set; }
+
+    public EmbeddingF32 Embedding { get; set; }
+}
--- SemanticSearchDemo/Program.cs
+++ SemanticSearchDemo/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace SemanticSearchDemo
+﻿using System.Diagnostics;
+using SmartComponents.LocalEmbeddings;
+using System.Text.Json;
+
+namespace SemanticSearchDemo
 {
     internal class Program
     {
+        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+        };
+
+        private static readonly HashSet<string> Categories = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase)
+        {
+            "POLITICS", "SCIENCE", "TECH", "WORLD NEWS", "TRAVEL"
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var embedder = new LocalEmbedder();
+            var lines = File.ReadLines(@"News.json");
+
+            var stopwatch = Stopwatch.StartNew();
+
+            int count = 0;
+
+            var newsItems = lines
+                .Select(line => JsonSerializer.Deserialize<NewsItem>(line, JsonOptions))
+                .Where(item => Categories.Contains(item.Category))
+                .Select(item =>
+                {
+                    item.Embedding = embedder.Embed(item.Headline);
+
+                    count++;
+                    if (count % 10000 == 0)
+                    {
+                        Console.WriteLine($"Indexed {count} items");
+                    }
+
+                    return item;
+                }).ToList();
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Indexed {newsItems.Count} items in {stopwatch.Elapsed}, enter search text {Environment.NewLine}");
+
+            do
+            {
+                var query = embedder.Embed(Console.ReadLine());
+                Console.WriteLine();
+
+                var results = LocalEmbedder.FindClosestWithScore(query, newsItems.Select(item => (item, item.Embedding)), 10);
+
+                foreach (var newsItem in results)
+                {
+                    Console.WriteLine($"{newsItem.Similarity}  {newsItem.Item.Headline}");
+                }
+                Console.WriteLine();
+
+            } while (true);
         }
     }
 }
